1aacf94cfd0e3f703496c47914ab189ee015bb0d
Pradeep <pradeepshivashimpi@gmail.com>
2024-08-06 23:16:21 +0530

    register API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,12 +3,9 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API;
+namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-
-public class UsersController(DataContext context) : ControllerBase
+public class UsersController(DataContext context) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
