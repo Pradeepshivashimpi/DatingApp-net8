0634944e314c2f5c89c6772e6aa4315d24d1a267
Pradeep <pradeepshivashimpi@gmail.com>
2024-08-11 00:02:53 +0530

    login and logout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,25 +2,39 @@ import { NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_Services/account.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgFor],
+  imports: [RouterOutlet, NgFor, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
+  private accountService = inject(AccountService);
   title = 'DatingApp';
   users: any;
 
   ngOnInit(): void {
+   this.getUsers();
+   this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.CurrentUser.set(user);
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log("request has been completed")
-      
     })
   }
 }
