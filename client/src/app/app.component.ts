b8b8fd7aa2689edb2fbef42bfc2523bd27c15ea9
Pradeep <pradeepshivashimpi@gmail.com>
2024-08-12 22:52:53 +0530

    home page and register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,21 @@
 import { NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_Services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NavComponent],
+  imports: [RouterOutlet, NgFor, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountService);
-  title = 'DatingApp';
-  users: any;
 
   ngOnInit(): void {
-   this.getUsers();
    this.setCurrentUser();
   }
 
@@ -30,11 +26,4 @@ export class AppComponent implements OnInit {
     this.accountService.CurrentUser.set(user);
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("request has been completed")
-    })
-  }
 }
