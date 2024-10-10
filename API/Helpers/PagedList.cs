504922f3c9d3fd846b5e4c6691e3e318db585c60
Pradeep <pradeepshivashimpi@gmail.com>
2024-10-10 21:18:49 +0530

    using the pagination clasess

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -23,7 +23,7 @@ public class PagedList<T> : List<T>
      int pageSize) 
    {
        var count = await source.CountAsync();
-       var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageNumber).ToListAsync();
+       var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
        return new PagedList<T>(items, count, pageNumber, pageSize);
    }
 }
