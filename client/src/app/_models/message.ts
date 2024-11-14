51af8422072aa98bd523bf559ee5fd034891f363
Pradeep <pradeepshivashimpi@gmail.com>
2024-11-14 22:22:36 +0530

    setting angular app for messaging

---
+++ client/src/app/_models/message.ts
@@ -0,0 +1,13 @@
+export interface Message {
+    id: number
+    senderId: number
+    senderUsername: string
+    senderPhotoUrl: string
+    receipientId: number
+    receipientUsername: string
+    receipientPhotoUrl: string
+    content: string
+    dateRead?: Date
+    messageSent: Date
+  }
+  
