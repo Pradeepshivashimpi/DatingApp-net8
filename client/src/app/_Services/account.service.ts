66169d60434b17f4a1048b372a2120793900a099
Pradeep <pradeepshivashimpi@gmail.com>
2024-12-14 22:52:15 +0530

    client side signalr

--- client/src/app/_Services/account.service.ts
+++ client/src/app/_Services/account.service.ts
@@ -4,6 +4,7 @@ import { User } from '../_models/user';
 import { map } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { LikesService } from './likes.service';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ import { LikesService } from './likes.service';
 export class AccountService {
   private http = inject(HttpClient);
   private likesService = inject(LikesService);
+  private presenceService = inject(PresenceService);
   baseUrl = environment.apiUrl;
   CurrentUser = signal<User | null> (null)
   roles = computed(() => {
@@ -49,12 +51,14 @@ export class AccountService {
     localStorage.setItem('user', JSON.stringify(user));
     this.CurrentUser.set(user);
     this.likesService.getLikeIds();
+    this.presenceService.createHubConnection(user);
   }
 
 
   logout() {
     localStorage.removeItem('user');
     this.CurrentUser.set(null);
+    this.presenceService.stopHubConnection();
   }
 
 }
