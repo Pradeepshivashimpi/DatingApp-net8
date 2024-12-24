3221eee9076300fe66c3dd5ae7565d2192fc6127
Pradeep <pradeepshivashimpi@gmail.com>
2024-12-24 22:57:56 +0530

    updating the message hub with the group tracking

--- API/Entities/Connection.cs
+++ API/Entities/Connection.cs
@@ -1,9 +1,7 @@
-using System;
-
 namespace API.Entities;
 
 public class Connection
 {
-   public required int ConnectionId { get; set; }
+   public required string ConnectionId { get; set; }
    public required string Username { get; set; }
 }
